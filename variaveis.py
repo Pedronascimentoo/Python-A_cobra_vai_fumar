432fbd7902254fb329e29f55a97da22c4018569b
PedroNacimento <122691587+Pedronascimentoo@users.noreply.github.com>
2023-06-25 21:03:15 -0300

    Add files via upload
    25-06-2023
    21;02
    002

---
+++ variaveis.py
@@ -0,0 +1,16 @@
+#declaraçãõ de uma variavel
+num = 0
+_num = 0
+num_= 0
+num_num=0
+#tipos de variaveis 
+num = 10
+print(num)
+print(type(num))
+#inteiro
+texto = "texto"
+print(type(texto))
+#string
+letras  = true
+print(type(letras))
+#booleano
